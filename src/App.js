635943fbbfdc9bf13fe14543002009a2a1d2fb92
YXR <yangxuru870918@gmail.com>
2022-04-27 23:20:44 +0800

    adjust

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './page/home'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route exact path="/" element={<Home/>}/>
         </Routes>
